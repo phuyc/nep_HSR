cf32de83f3128e536a3fe834dc6454b5c5d809ce
phuyc <lhphuc70@gmail.com>
2023-04-24 22:33:04 +0700

    Fix a bug where sometimes the result will carry on to the next rolls

--- commands/warp.js
+++ commands/warp.js
@@ -21,7 +21,7 @@ module.exports = {
             option.setName('banner')
                 .setDescription('Type of banner')
                 .setChoices(
-                    { name: 'Character Oriented Banner', value: 'characters'},
+                    { name: 'Character Oriented Banner', value: 'banner'},
                     { name: 'Light Cones Oriented Banner', value: 'light_cones'},
                     { name: 'Standard Banner', value: 'standard'},
                 )
@@ -48,44 +48,33 @@ module.exports = {
             const images = [];
             for (let i = 0; i < results.length; i++) {
                 // ? Add image along with the coordinate to composite
-                    if (i < 3) {
-                        images.push({
-                            input: `./images/assets/cards/${results[i]}.png`,
-                            top: 266,
-                            left: 71 + 508 * i
-                        });
-                    } else if (i < 7) {
-                        images.push({
-                            input: `./images/assets/cards/${results[i]}.png`,
-                            top: 586,
-                            left: -61 + 508 * (i - 3)
-                        });
-                    } else {
-                        images.push({
-                            input: `./images/assets/cards/${results[i]}.png`,
-                            top: 906,
-                            left: 321 + 508 * (i - 7)
-                        });
+                        if (i < 3) {
+                            images.push({
+                                input: `./images/assets/rotated_cards/${results[i]}.png`,
+                                top: 60 - 69 * i,
+                                left: -90 + 254 * i
+                            });
+                        } else if (i < 7) {
+                            images.push({
+                                input: `./images/assets/rotated_cards/${results[i]}.png`,
+                                top: 230 - 69 * (i - 3),
+                                left: -100 + 254 * (i - 3)
+                            });
+                        } else {
+                            images.push({
+                                input: `./images/assets/rotated_cards/${results[i]}.png`,
+                                top: 330 - 69 * (i - 7),
+                                left: 130 + 253 * (i - 7)
+                            });
+                        }
                     }
-                }
-            // Composite image
-            await sharp("./images/assets/card_template/rotated_background.png")
-                .composite(images)
-                .toFile(`tmp_${banner}.png`);
+            
+                    // Composite image
+                    await sharp("./images/assets/card_template/small_background.png")
+                        .composite(images)
+                        .toFile(`tmp_${banner}.png`);
 
-            // Rotate image
-            await sharp(`./tmp_${banner}.png`)
-                .rotate(-15)
-                .extract(
-                    {
-                        left: 274,
-                        top: 487,
-                        width: 1948,
-                        height: 1096,
-                })
-                .toFile(`./gacha_${banner}.png`);
-
-                const file = new AttachmentBuilder(`./gacha_${banner}.png`);
+                const file = new AttachmentBuilder(`./tmp_${banner}.png`);
 
                 let pity5 = db.prepare(`SELECT pity_5_${banner} FROM users WHERE user_id=?`).get(interaction.user.id);
                 pity5 = pity5[`pity_5_${banner}`];
